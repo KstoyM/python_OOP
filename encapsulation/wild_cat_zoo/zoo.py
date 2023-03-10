ee61e94e170e3b983a0d038eb10a3f710374639c
KstoyM <k.stoyanov911@gmail.com>
2023-03-10 08:36:47 +0200

    solved tasks

---
+++ encapsulation/wild_cat_zoo/zoo.py
@@ -0,0 +1,96 @@
+from typing import List
+
+from wild_cat_zoo.animal import Animal
+from wild_cat_zoo.worker import Worker
+
+
+class Zoo:
+
+    def __init__(self, name: str, budget: int, animal_capacity: int, workers_capacity: int):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+        self.animals: List[Animal] = []
+        self.workers: List[Worker] = []
+
+    def add_animal(self, animal: Animal, price: float):
+        if self.__budget < price:
+            return "Not enough budget"
+        elif self.__animal_capacity == len(self.animals):
+            return "Not enough space for animal"
+
+        self.__budget -= price
+        self.animals.append(animal)
+
+        return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
+
+    def hire_worker(self, worker):
+        if self.__workers_capacity == len(self.workers):
+            return "Not enough space for worker"
+
+        self.workers.append(worker)
+        return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+
+    def fire_worker(self, worker_name):
+        try:
+            worker = next(filter(lambda w: w.name == worker_name, self.workers))
+            self.workers.remove(worker)
+            return f"{worker_name} fired successfully"
+        except StopIteration:
+            return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        salaries = sum(worker.salary for worker in self.workers)
+
+        if self.__budget < salaries:
+            return "You have no budget to pay your workers. They are unhappy"
+
+        self.__budget -= salaries
+        return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+
+    def tend_animals(self):
+        money_needed = sum(animal.money_for_care for animal in self.animals)
+
+        if self.__budget < money_needed:
+            return "You have no budget to tend the animals. They are unhappy."
+
+        self.__budget -= money_needed
+        return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+
+    def profit(self, amount):
+        self.__budget += amount
+
+    def animals_status(self):
+        lions = list(filter(lambda a: a.__class__.__name__ == "Lion", self.animals))
+        tigers = list(filter(lambda a: a.__class__.__name__ == "Tiger", self.animals))
+        cheetahs = list(filter(lambda a: a.__class__.__name__ == "Cheetah", self.animals))
+
+        result = [
+            f"You have {len(self.animals)} animals",
+            f"----- {len(lions)} Lions:",
+        ]
+        result.extend(lions)
+
+        result.append(f"----- {len(tigers)} Tigers:")
+        result.extend(tigers)
+        result.append(f"----- {len(cheetahs)} Cheetahs:")
+        result.extend(cheetahs)
+
+        return "\n".join(str(x) for x in result)
+
+    def workers_status(self):
+        info = {"Keeper": [], "Caretaker": [], "Vet": []}
+        [info[w.__class__.__name__].append(str(w)) for w in self.workers]
+
+        result = [
+            f"You have {len(self.workers)} workers",
+            f"----- {len(info['Keeper'])} Keepers:",
+            *info['Keeper'],
+            f"----- {len(info['Caretaker'])} Caretakers:",
+            *info['Caretaker'],
+            f"----- {len(info['Vet'])} Vets:",
+            *info['Vet']
+        ]
+
+        return "\n".join(result)
