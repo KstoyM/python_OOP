da9f45070915b6fde05cff934ee9c9f40967354a
KstoyM <k.stoyanov911@gmail.com>
2023-03-23 19:58:07 +0200

    solved tasks

---
+++ decorators/store_results_2.py
@@ -0,0 +1,22 @@
+class store_results:
+
+    def __init__(self, arg):
+        self.arg = arg
+
+    def __call__(self, func):
+        def wrapper(*args):
+            print(f"Function {func.__name__} was called. Result: {func(*args)}")
+
+        return wrapper
+
+
+@store_results(6)
+def add(a, b):
+    return a + b
+
+@store_results(5)
+def mult(a, b):
+    return a * b
+
+add(2, 2)
+mult(6, 4)
